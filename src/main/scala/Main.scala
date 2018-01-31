5bf45e408f089822e389b1cde1a396f0002e57f7
Unknown <msuwanchodratsamee@gmail.com>
2018-01-31 23:25:52 +0700

    Added request validation

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 import com.google.inject.Guice
 import models.Cash
 import services.AtmService
+import utils.{CustomException, Validations}
 
 import scala.io.StdIn
 
@@ -19,24 +20,29 @@ object Main extends App {
       val wantedCash: Long = StdIn.readLine("How much do you want to withdraw?(positive number without decimal only): ").toLong
       println
 
-      atmService.withdraw(Cash(wantedCash)) match {
-        case Left(e) => {
-          flag = false
+      if(Validations.isValidRequest(Cash(wantedCash))) {
+        atmService.withdraw(Cash(wantedCash)) match {
+          case Left(e) => {
+            flag = false
 
-          val message = e.getMessage.replaceAllLiterally("requirement failed: ", "")
+            val message = e.getMessage.replaceAllLiterally("requirement failed: ", "")
 
-          println(message)
+            println(message)
+          }
+          case Right(notes) => {
+            val message = s"You've got ${notes.thousand} thousand notes, ${notes.five_hundred} five hundred notes, ${notes.hundred} hundred notes, ${notes.fifty} fifty notes and ${notes.twenty} twenty notes."
+
+            println(message)
+            println("Thank you.")
+            println
+          }
         }
-        case Right(notes) => {
-          val message = s"You've got ${notes.thousand} thousand notes, ${notes.five_hundred} five hundred notes, ${notes.hundred} hundred notes, ${notes.fifty} fifty notes and ${notes.twenty} twenty notes."
-
-          println(message)
-          println("Thank you.")
-          println
-        }
+      } else {
+        throw CustomException(s"In an ATM with only 20, 50, 100, 500 and 1000 notes, it is not possible to dispense $wantedCash")
       }
     }
   } catch {
+    case e: CustomException => println(e.getMessage)
     case e: Exception => println("Positive number without decimal only!")
   }
 }
