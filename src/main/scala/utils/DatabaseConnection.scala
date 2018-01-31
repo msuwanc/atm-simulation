f922b9d600fec51c9f6188c3c328a58c8ce146c6
Unknown <msuwanchodratsamee@gmail.com>
2018-01-31 22:49:05 +0700

    Added configuration in the application for flexibility

--- src/main/scala/utils/DatabaseConnection.scala
+++ src/main/scala/utils/DatabaseConnection.scala
@@ -3,13 +3,7 @@ package utils
 import models.{Atm, Notes}
 
 class DatabaseConnection {
-  val twentyNotes = 5
-  val fiftyNotes = 2
-  val hundredNotes = 8
-  val fiveHundredNotes = 2
-  val thousandNotes = 98
-
-  val notes: Notes = Notes(twentyNotes, fiftyNotes, hundredNotes, fiveHundredNotes, thousandNotes)
+  val notes: Notes = Notes(Configurations.twentyNotes, Configurations.fiftyNotes, Configurations.hundredNotes, Configurations.fiveHundredNotes, Configurations.thousandNotes)
 
   var atm: Atm = Atm(notes, Calculation.calculateAvailableCash(notes))
 }
